ea4cc90a2a61f8a73bdfa9615a466caa8794f4a5
OlofBorendal <Olof.Borendal@gmail.com>
2025-12-05 08:36:37 +0100

    h

--- init.js
+++ init.js
@@ -1,1 +1,35 @@
  // Extrauppgift: Fixa så att headern och footern laddas in i varje sida från denna fil
+
+
+let header = document.getElementsByTagName("Header");
+ 
+ header[0].innerHTML = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.html">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="movies.html">Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="music.html">Music</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="series.html">Series</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+`
+
+
+
+
+
+
+
